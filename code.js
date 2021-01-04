ce1d57f3b72b895c6b20fab4e444dd35b2c02b78
Aarya201 <76944790+Aarya201@users.noreply.github.com>
2021-01-04 15:59:53 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+function ci() {
+    document.getElementById("sdb").src = "https://www.jagranjosh.com/imported/images/E/Articles/bradman-records.png"; 
+}
+function r() {
+    document.getElementById("r").innerHTML = "<h2>6,996</h2>";
+}
+function a() {
+    document.getElementById("a").innerHTML = "<h2>99.94</h2>";
+}
+function s() {
+    document.getElementById("s").innerHTML = "<h2>71.4</h2>";
+}
+function f() {
+    document.getElementById("f").innerHTML = "<h2>His test Average of 99.94 has been the highest achievment by anyone in any major sport</h2>";
+}
